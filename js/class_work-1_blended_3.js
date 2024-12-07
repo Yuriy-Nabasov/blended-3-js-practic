78a192ac36847960dc1282e1bfd4046bfbe1d0cc
Yuriy Nabasov <nabasoff@gmail.com>
2024-12-07 20:05:34 +0200

    leson 07-12-2024

--- js/class_work-1_blended_3.js
+++ js/class_work-1_blended_3.js
@@ -495,3 +495,248 @@
 // console.log(
 //   `Студент з найменшим віком: ${yangerStudent.name}, Вік: ${yangerStudent.age}, Спеціальність: ${yangerStudent.major}`
 // );
+
+//! 07-12-2024
+//? Напишіть функцію sayHi(), яка приймає в якості параметра масив імен,
+//? перебирає масив і додає до кожного ім'я привітання.
+//? Функція повертає новий масив  ["Ivan привіт!", "Maria привіт!", "Anna привіт!"]
+
+// const names = ["Ivan", "Maria", "Anna"];
+
+// const sayHi = (names) => names.map((name) => `${name} привіт!`);
+
+// console.log(names);
+// console.log(sayHi(names));
+
+//? Напиши функцію, котра приймає масив користувачів і id.
+//? У масиві користувачів потрібно знайти людину з заданим id
+//? і повернути його ім'я. Якщо користувача не було знайдено
+//? функція повертає повідомлення
+//? "Користувача з таким id не знайдено"
+
+// const users = [
+//   { id: 1, name: "John" },
+//   { id: 2, name: "Pete" },
+//   { id: 3, name: "Mary" },
+// ];
+
+// const fn = (arr, id) => {
+//   const result = arr.find((user) => user.id === id);
+//   return result
+//     ? `Ім'я користувача з id=${id} - ${result.name}`
+//     : `Користувача з таким id=${id} не знайдено`;
+// };
+
+// console.log(fn(users, 1));
+// console.log(fn(users, 2));
+// console.log(fn(users, 3));
+// console.log(fn(users, 4));
+
+//? Виконати фільтрацію, залишивши в allTopics тільки унікальні елементи.
+
+// const courses = [
+//   {
+//     name: "Basic HTML+CSS",
+//     topics: ["VSCode", "HTML", "CSS", "GitHub", "GitHub Desctop"],
+//   },
+//   {
+//     name: "Intermediate HTML+CSS",
+//     topics: ["VSCode", "HTML", "CSS", "GitHub", "Git", "Terminal"],
+//   },
+//   {
+//     name: "Basic JavaScript",
+//     topics: [
+//       "VSCode",
+//       "Type system",
+//       "Loops",
+//       "Function",
+//       "Git",
+//       "Conditions",
+//       "Classes",
+//       "GitHub",
+//       "DOM",
+//     ],
+//   },
+//   {
+//     name: "Intermediate JavaScript",
+//     topics: [
+//       "VSCode",
+//       "NPM",
+//       "Bundlers",
+//       "Transpiling",
+//       "Git",
+//       "Promises",
+//       "AJAX",
+//       "GitHub",
+//     ],
+//   },
+// ];
+
+// const allTopics = courses
+//   .flatMap((item) => item.topics)
+//   .filter((item, index, array) => array.indexOf(item) === index);
+// console.table(allTopics);
+
+//? Напишіть функцію getGirlsPassports() яка поверне масив номерів паспортів дівчат.
+// const friends = [
+//   {
+//     passport: "03005988",
+//     name: "Joseph Francis Tribbiani Jr",
+//     age: 32,
+//     sex: "m",
+//   },
+//   { passport: "03005989", name: "Chandler Muriel Bing", age: 33, sex: "m" },
+//   { passport: "03005990", name: "Ross Eustace Geller", age: 33, sex: "m" },
+//   { passport: "03005991", name: "Rachel Karen Green", age: 31, sex: "f" },
+//   { passport: "03005992", name: "Monica Geller", age: 31, sex: "f" },
+//   { passport: "03005993", name: "Phoebe Buffay", age: 34, sex: "f" },
+// ];
+
+// const getGirlsPassports = function (friends) {
+//   return friends.filter((el) => el.sex === `f`).map((el) => el.passport);
+// };
+
+// console.log(getGirlsPassports(friends));
+
+//? Mаємо масив об'єктів, який представляє список товарів і їх цін:
+//? Завдання: Знайдіть суму вартості всіх товарів, які коштують більше 10 гривень.
+
+// const products = [
+//   { name: "Milk", price: 20 },
+//   { name: "Bread", price: 10 },
+//   { name: "Eggs", price: 15 },
+// ];
+
+// const result = products
+//   .filter((product) => product.price > 10)
+//   .reduce((acum, product) => product.price + acum, 0);
+// console.log(result);
+
+//? Є два об'єкти, які описують деякі характеристики телефонів.
+//? Потрібно викликати метод showInfo, щоб він логував інформацію про кожний телефон,
+//? передаючи аргументом країну-виробник телефону.
+//? "HUAWEI" - "China", "SAMSUNG" - "South Korea".
+
+// const tel = {
+//   regNumber: "AE8345FEDAS",
+//   brand: "HUAWEI",
+//   showInfo(country) {
+//     console.log(`${country} ${this.regNumber} ${this.brand}`);
+//   },
+// };
+
+// const tel_2 = {
+//   regNumber: "AO8518FEGHT",
+//   brand: "SAMSUNG",
+// };
+
+// tel.showInfo(`China`);
+// tel.showInfo.call(tel_2, `South Korea`);
+// tel.showInfo.apply(tel_2, [`South Korea`]);
+
+// const telInfo = tel.showInfo.bind(tel_2, `South Korea`);
+
+// telInfo();
+
+//? Напиши клас Rectangle який створює об'єкт
+//? з ​​властивостями height, width
+//? і методом calculateArea() для підрахунку площі прямокутника.
+
+// class Rectangle {
+//   constructor(height, width) {
+//     this.height = height;
+//     this.width = width;
+//   }
+//   calculateArea() {
+//     return this.height * this.width;
+//   }
+// }
+
+// const rectangle = new Rectangle(5, 10);
+
+// console.log(rectangle.calculateArea());
+
+//? Створіть клас Car, який має конструктор для зберігання марки та
+//? року виробництва автомобіля.
+//? 1. Додайте метод, який обчислює вік автомобіля на основі поточного року
+//? та року виробництва і виведе в кнсоль рядок `Вік автомобіля: age років`
+//? 2. Додайте перевірку, якщо вік автомобіля більше ніж 5 років, виводимо
+//? повідомлення "Автомобіль потребує технічного обслуговування."
+//? в іншому випадку - "Автомобіль не потребує технічного обслуговування зараз."
+//? 3. Додайте перевірку, якщо рік виробництва перевищює поточний рік,
+//? вивести повідомлення "Помилка: Рік виробництва не може бути пізнішим за поточний рік."
+
+// class Car {
+//   constructor(brand, year) {
+//     this.brand = brand;
+//     this.year = year;
+//   }
+//   calcYears() {
+//     const curentYear = new Date().getFullYear();
+//     if (this.year > curentYear) {
+//       console.log(
+//         `Помилка: Рік виробництва не може бути пізнішим за поточний рік.`
+//       );
+//       return;
+//     }
+
+//     const age = curentYear - this.year;
+//     console.log(`Вік автомобіля: ${age} років`);
+//     age > 5
+//       ? console.log(`Автомобіль потребує технічного обслуговування.`)
+//       : console.log(`Автомобіль не потребує технічного обслуговування зараз.`);
+//   }
+// }
+// const test1Car = new Car(`BMV`, 2030);
+// console.log(test1Car);
+// test1Car.calcYears();
+
+// const test2Car = new Car(`Audi`, 2017);
+// console.log(test2Car);
+// test2Car.calcYears();
+
+// const test3Car = new Car(`Skoda`, 2020);
+// console.log(test3Car);
+// test3Car.calcYears();
+
+// const test4Car = new Car(`Nissan`, 2023);
+// console.log(test4Car);
+// test4Car.calcYears();
+
+//? Напиши клас Client який створює об'єкт
+//? з ​​властивостями login, email
+//? Оголоси приватні властивості #login #email,
+//? доступ до яких зроби через геттер та сеттер login email
+
+// class Client {
+//   #login;
+//   #email;
+//   constructor(login, email) {
+//     this.#login = login;
+//     this.#email = email;
+//   }
+//   get login() {
+//     return this.#login;
+//   }
+//   get email() {
+//     return this.#email;
+//   }
+
+//   set login(newLogin) {
+//     this.#login = newLogin;
+//   }
+//   set email(newEmail) {
+//     this.#email = newEmail;
+//   }
+// }
+
+// const clientIvan = new Client(`Ivan`, `ivan@gmail.com`);
+
+// console.log(clientIvan.login);
+// console.log(clientIvan.email);
+
+// clientIvan.login = `newIvan`;
+// console.log(clientIvan.login);
+
+// clientIvan.email = `newivan@gmail.com`;
+// console.log(clientIvan.email);
